9285efd565b2af739c3817096e9f883feb564325
itsjh <93984898+itsjh1242@users.noreply.github.com>
2023-04-26 00:09:54 +0900

    1차 병합
    관리자 페이지 추가
    메인 대시보드,
    연구원 관리

--- Server/app.js
+++ Server/app.js
@@ -6,9 +6,10 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
+var mainRouter = require("./routes/main");
 var signRouter = require("./routes/sign");
 var checkRouter = require("./routes/check");
+var adminRouter = require("./routes/admin");
 
 var app = express();
 
@@ -24,9 +25,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/", mainRouter);
 app.use("/sign", signRouter);
 app.use("/check", checkRouter);
+app.use("/admin", adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
