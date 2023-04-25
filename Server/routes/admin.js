cc90d830f4a0c531311b2318518f52ad7aefaede
itsjh <93984898+itsjh1242@users.noreply.github.com>
2023-04-26 06:29:39 +0900

    2차 머지

--- Server/routes/admin.js
+++ Server/routes/admin.js
@@ -3,12 +3,7 @@ const router = express.Router();
 const pool = require("../db/db");
 const sessionStore = require("../db/session");
 
-const today = new Date();
-const date = {
-    year: today.getFullYear(),
-    month: today.getMonth(),
-    date: today.getDate(),
-};
+const moment = require("moment");
 
 let data = {
     title: "융합연구소출석관리서비스 | 출석체크",
@@ -24,53 +19,99 @@ let header_data = {
     unapprovedExcused: 0,
 };
 
-let panel_user = {
-    users: [],
-};
 
 // 관리자페이지 GET
 router.get("/", async (req, res, next) => {
     if (req.session.isLogined == undefined) {
         res.redirect("/sign");
     } else {
-        data.uid = req.session.uid;
-        data.uname = req.session.uname;
+        try {
+            data.uid = req.session.uid;
+            data.uname = req.session.uname;
+            // 관리자 페이지 메인 탭 Header에 들어갈 데이터 가져오기
+            // 전체 연구원 수, 출석 연구원 수, 미출석 연구원 수, 미승인 유고결석 개수
+            // 전체 연구원 수
+            const getAllUser = await pool.query(
+                "SELECT COUNT(*) AS 'users' FROM user"
+            );
+            header_data.users = getAllUser[0][0].users;
+            // 금일 출석한 연구원 수, 미출석 연구원 수
+            const getAttendedUser = await pool.query(
+                "SELECT COUNT(*) AS 'users' FROM check_in WHERE DATE_FORMAT(date, '%Y-%m-%d') = CURDATE()"
+            );
+            header_data.attendedUsers = getAttendedUser[0][0].users;
+            header_data.absentedUsers =
+                header_data.users - header_data.attendedUsers;
+            // 미승인 유고결석 개수
+            const getUnapprovedExcused = await pool.query(
+                "SELECT COUNT(*) AS 'unapproved' FROM excused_absence WHERE status = ?",
+                ["승인 대기중"]
+            );
+            header_data.unapprovedExcused = getUnapprovedExcused[0][0].unapproved;
 
-        // 관리자 페이지 메인 탭 Header에 들어갈 데이터 가져오기
-        // 전체 연구원 수, 출석 연구원 수, 미출석 연구원 수, 미승인 유고결석 개수
-        // 전체 연구원 수
-        const getAllUser = await pool.query(
-            "SELECT COUNT(*) AS 'users' FROM user"
-        );
-        header_data.users = getAllUser[0][0].users;
-        // 금일 출석한 연구원 수, 미출석 연구원 수
-        const getAttendedUser = await pool.query(
-            "SELECT COUNT(*) AS 'users' FROM check_in WHERE DATE_FORMAT(date, '%Y-%m-%d') = CURDATE()"
-        );
-        header_data.attendedUsers = getAttendedUser[0][0].users;
-        header_data.absentedUsers =
-            header_data.users - header_data.attendedUsers;
-        // 미승인 유고결석 개수
-        const getUnapprovedExcused = await pool.query(
-            "SELECT COUNT(*) AS 'unapproved' FROM excused_absence WHERE status = ?",
-            ["승인 대기중"]
-        );
-        header_data.unapprovedExcused = getUnapprovedExcused[0][0].unapproved;
-
-        // 관리자 페이지 연구원 관리 탭에 들어갈 데이터 가져오기
-        // 전체 연구원, 출석 상태 확인을 위한 오늘 날짜의 출석 데이터
-        const getAllMember = await pool.query(
-            "SELECT a.uid, a.uname, a.upw, a.seat, DATE_FORMAT(b.date, '%H시%i분%s초') AS 'in', DATE_FORMAT(c.date, '%H시%i분%초') AS 'out' FROM user AS a LEFT JOIN check_in AS b ON a.uid = b.uid AND DATE_FORMAT(b.date, '%Y-%m-%d') = CURDATE() LEFT JOIN check_out AS c ON a.uid = c.uid AND DATE_FORMAT(c.date, '%Y-%m-%d') = CURDATE();"
-        );
-        panel_user = getAllMember[0];
-        // 입실 시간, 퇴실 시간
-        return res.render("admin", {
-            data: data,
-            header_data: header_data,
-            panel_user: panel_user,
-        });
+            return res.render("admin", {
+                data: data,
+                header_data: header_data
+            });
+        } catch (error) {
+            console.log(error);
+        }
     }
 });
+
+// Admin-User
+router.get("/admin-user", async (req, res, next) => {
+    if (req.session.isLogined == undefined) {
+        res.redirect("/sign");
+    } else {
+        try {
+            data.uid = req.session.uid;
+            data.uname = req.session.uname;
+            let key = req.query.key;
+            if (key == undefined) {
+                key = moment().format("YYYY-MM-DD");
+            }
+            // 관리자 페이지 연구원 관리 탭에 들어갈 데이터 가져오기
+            // 전체 연구원, 출석 상태 확인을 위한 오늘 날짜의 출석 데이터
+            const getAllMember = await pool.query(
+                "SELECT a.uid, a.uname, a.upw, a.seat, DATE_FORMAT(b.date, '%H시%i분%s초') AS 'in', DATE_FORMAT(c.date, '%H시%i분%s초') AS 'out' FROM user AS a LEFT JOIN check_in AS b ON a.uid = b.uid AND DATE_FORMAT(b.date, '%Y-%m-%d') = ? LEFT JOIN check_out AS c ON a.uid = c.uid AND DATE_FORMAT(c.date, '%Y-%m-%d') = ?;",
+                [key, key]
+            );
+            // console.log(getAllMember[0].length)
+            return res.render("admin-user", {
+                data: data,
+                bodydata: getAllMember[0],
+                key: key
+            });
+        } catch (error) {
+            console.log(error);
+            return res.redirect("/error"); 
+        }
+    }
+})
+
+// Admin-Absent
+router.get("/admin-absent", async (req, res, next) => {
+    if (req.session.isLogined == undefined) {
+        res.redirect("/sign");
+    } else {
+        try {
+            data.uid = req.session.uid;
+            data.uname = req.session.uname;
+            // 유고결석 테이블 데이터 가져오기
+            const getAbsentData = await pool.query(
+                "SELECT a.no, a.uid, b.uname, a.status, a.wdate, a.context, a.file FROM excused_absence AS a LEFT JOIN user AS b ON a.uid = b.uid ORDER BY no DESC"
+            );
+            return res.render("admin-absent", {
+                data: data,
+                bodydata: getAbsentData[0]
+            });
+        } catch (error) {
+            console.log(error);
+            return res.redirect("/error"); 
+        }
+    }
+})
 
 // 관리자 페이지 사용자 추가
 router.post("/adduser", async (req, res, next) => {
@@ -96,7 +137,9 @@ router.post("/adduser", async (req, res, next) => {
                     "INSERT INTO user VALUES (?, ?, ?, ?)",
                     [studentcode, userpwd, username, userseat]
                 );
-                return res.redirect("/admin");
+                return res.send(
+                    "<script>alert('사용자를 추가했습니다.'); location.href='/admin/admin-user';</script>"
+                );
             } catch (error) {
                 return res.redirect("/");
             }
@@ -116,16 +159,18 @@ router.post("/edituser", async (req, res, next) => {
             "UPDATE user SET uid = ?, upw = ?, uname = ?, seat = ? WHERE uid = ?",
             [studentcode, userupw, username, userseat, req.session.uid]
         );
-        return res.redirect("/admin");
+        return res.send(
+            "<script>alert('사용자 수정을 완료했습니다.'); location.href='/admin/admin-user';</script>"
+        );
     }
 });
 
 // 관리자 페이지 사용자 삭제
 router.post("/deleteuser", async (req, res, next) => {
-    try {
-        if (req.session.isLogined == undefined) {
-            res.redirect("/sign");
-        } else {
+    if (req.session.isLogined == undefined) {
+        res.redirect("/sign");
+    } else {
+        try {
             const {
                 body: { studentcode },
             } = req;
@@ -135,12 +180,55 @@ router.post("/deleteuser", async (req, res, next) => {
                 "DELETE FROM user WHERE uid = ?",
                 [studentcode]
             );
-            return res.redirect("/admin");
+            return res.send(
+                "<script>alert('사용자를 삭제했습니다.'); location.href='/admin/admin-user';</script>"
+            );
+        } catch (error) {
+            return res.redirect("/error");
         }
-    } catch (error) {
-        console.log(error);
-        // Error Page
     }
 });
 
+// 유고결석 승인
+router.post("/accessabsent", async (req, res, next) => {
+    if (req.session.isLogined == undefined) {
+        res.redirect("/sign");
+    } else {
+        try {
+            const {
+                body: { no },
+            } = req;
+            const updateStatus = await pool.query(
+                "UPDATE excused_absence SET status = '승인 완료' WHERE no = ?",
+                [no]
+            )
+            return res.send(
+                "<script>alert('유고결석을 승인했습니다.'); location.href='/admin/admin-absent';</script>"
+            );
+        } catch (error) {
+            return res.redirect("/error");
+        }
+    }
+})
+// 유고결석 거절
+router.post("/denyabsent", async (req, res, next) => {
+    if (req.session.isLogined == undefined) {
+        res.redirect("/sign");
+    } else {
+        try {
+            const {
+                body: { no },
+            } = req;
+            const updateStatus = await pool.query(
+                "UPDATE excused_absence SET status = '승인 거절' WHERE no = ?",
+                [no]
+            )
+            return res.send(
+                "<script>alert('유고결석을 거절했습니다.'); location.href='/admin/admin-absent';</script>"
+            );
+        } catch (error) {
+            return res.redirect("/error");
+        }
+    }
+})
 module.exports = router;
