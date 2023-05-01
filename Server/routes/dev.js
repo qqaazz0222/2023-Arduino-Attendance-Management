a0815f5116904616ea812c19330b14e1a8c3a85a
qqaazz0222 <qqaazz0222@naver.com>
2023-05-01 11:44:09 +0900

    추가 : 주석 작성

--- Server/routes/dev.js
+++ Server/routes/dev.js
@@ -10,14 +10,18 @@ let data = {
     errMsg: "",
 };
 
+// ** 해당 페이지는 아두이노로 디바이스 개발 완료시, 사용하지 않을 예정
+// 인원 출석, 퇴실 시간 설정 페이지 접속
 router.get("/", async (req, res, next) => {
     if (req.session.isLogined == undefined) {
         res.redirect("/sign");
     } else {
+        // 관리자가 아니면 메인 페이지로 이동
         if (req.session.isAdmin != true) {
             res.redirect("/");
         } else {
             try {
+                // 오늘 날짜 받아와 yyyy-mm-dd 형식으로 변경
                 const date = new Date();
                 const year = date.getFullYear();
                 const month = date.getMonth() + 1;
@@ -29,10 +33,12 @@ router.get("/", async (req, res, next) => {
                 }
                 let dd = String(day);
 
+                // DB에서 출석 정보 가져오기 
                 const check_in = await pool.query(
                     "WITH TEMP_TABLE as (SELECT * FROM check_in WHERE DATE_FORMAT(date, '%Y-%m-%d') = ?) SELECT user.uid, user.uname, TEMP_TABLE.no, TEMP_TABLE.date FROM user LEFT JOIN TEMP_TABLE ON user.uid = TEMP_TABLE.uid ORDER BY user.uid;",
                     [yyyy + "-" + mm + "-" + dd]
                 );
+                // DB에서 퇴실 정보 가져오기
                 const check_out = await pool.query(
                     "WITH TEMP_TABLE as (SELECT * FROM check_out WHERE DATE_FORMAT(date, '%Y-%m-%d') = ?) SELECT user.uid, user.uname, TEMP_TABLE.no, TEMP_TABLE.date FROM user LEFT JOIN TEMP_TABLE ON user.uid = TEMP_TABLE.uid ORDER BY user.uid;",
                     [yyyy + "-" + mm + "-" + dd]
@@ -58,6 +64,7 @@ router.post("/save", async (req, res, next) => {
         res.redirect("/");
     } else {
         try {
+            // 오늘 날짜 받아와 yyyy-mm-dd 형식으로 변경
             const { uid, intime, outtime } = req.body;
             console.log(typeof outtime);
             const date = new Date();
@@ -70,13 +77,16 @@ router.post("/save", async (req, res, next) => {
                 mm = "0" + mm;
             }
             let dd = String(day);
+            // 출석 시간에 대한 처리
             if (intime != "") {
+                // 해당 일에 이미 출석 시간이 저장되어 있는 지 확인
                 const check_in_already = await pool.query(
                     "SELECT * FROM check_in WHERE uid = ? AND DATE_FORMAT(date, '%Y-%m-%d') = ?;",
                     [uid, yyyy + "-" + mm + "-" + dd]
                 );
-
+                // 저장되어 있는 출석 시간이 있을 때 처리
                 if (check_in_already[0].length != 0) {
+                    // DB에 새로 입력받은 출석 시간을 업데이트
                     const chg_in_time = await pool.query(
                         "UPDATE check_in SET date = ? WHERE no = ?;",
                         [
@@ -84,19 +94,25 @@ router.post("/save", async (req, res, next) => {
                             check_in_already[0][0].no,
                         ]
                     );
+                // 저장되어 있는 출석 시간이 없을 때 처리
                 } else {
+                    // DB에 새로 입력받은 출석 시간을 삽입
                     const add_in_time = await pool.query(
                         "INSERT INTO check_in VALUES (null, ?, ?);",
                         [yyyy + "-" + mm + "-" + dd + " " + intime + ":00", uid]
                     );
                 }
             }
+            // 퇴실 시간에 대한 처리
             if (outtime != "") {
+                // 해당 일에 이미 퇴실 시간이 저장되어 있는 지 확인
                 const check_out_already = await pool.query(
                     "SELECT * FROM check_out WHERE uid = ? AND DATE_FORMAT(date, '%Y-%m-%d') = ?;",
                     [uid, yyyy + "-" + mm + "-" + dd]
                 );
+                // 저장되어 있는 퇴실 시간이 있을 때 처리
                 if (check_out_already[0].length != 0) {
+                    // DB에 새로 입력받은 퇴실 시간을 업데이트
                     const chg_out_time = await pool.query(
                         "UPDATE check_out SET date = ? WHERE no = ?;",
                         [
@@ -104,7 +120,9 @@ router.post("/save", async (req, res, next) => {
                             check_out_already[0][0].no,
                         ]
                     );
+                // 저장되어 있는 퇴실 시간이 없을 때 처리
                 } else {
+                    // DB에 새로 입력받은 퇴실 시간을 삽입
                     const add_out_time = await pool.query(
                         "INSERT INTO check_out VALUES (null, ?, ?);",
                         [
--- Server/routes/sign.js
+++ Server/routes/sign.js
@@ -10,9 +10,11 @@ let data = {
     errMsg: "",
 };
 
+// 로그인 페이지 접속
 router.get("/", async (req, res, next) => {
     try {
         if (req.session.uid) {
+            // 세션 삭제
             delete req.session.uid;
             delete req.session.uname;
             delete req.session.isLogined;
@@ -22,6 +24,7 @@ router.get("/", async (req, res, next) => {
                 res.redirect("/");
             });
         } else {
+            // 로그인 페이지로 이동
             res.render("sign", { data: data });
         }
     } catch (error) {
@@ -30,16 +33,21 @@ router.get("/", async (req, res, next) => {
     }
 });
 
+// 로그인 동작
 router.post("/", async (req, res, next) => {
     const { id, pw } = req.body;
     try {
+        // DB에서 회원 정보 가져오기
         const user = await pool.query("SELECT * FROM user WHERE uid = ?", [id]);
+        // 아이디 유효상 검사
         if (user[0].length > 0) {
+            // 비밀번호 일치 검사
             if (pw === user[0][0].upw) {
                 const admin = await pool.query(
                     "SELECT * FROM admin WHERE aid = ?",
                     [id]
                 );
+                // 관리자 확인
                 if (admin[0] != 0) {
                     req.session.isAdmin = true;
                 } else {
@@ -53,10 +61,12 @@ router.post("/", async (req, res, next) => {
                     res.redirect("/");
                 });
             } else {
+                // 비밀번호 불일치
                 data.errMsg = "* 잘못된 비밀번호입니다. 다시 확인하세요.";
                 res.render("sign", { data: data });
             }
         } else {
+            // 아이디 없음
             data.errMsg = "* 아이디가 존재하지 않습니다.";
             res.render("sign", { data: data });
         }
